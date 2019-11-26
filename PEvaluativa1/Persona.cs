2339f903d7f29e906a641adf082920770dc1cc4c
MerioUwU <54387601+MerioUwU@users.noreply.github.com>
2019-11-26 11:55:29 -0800

    Programa funcionando correctamente despues de ajustes

---
+++ PEvaluativa1/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEvaluativa1
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Profesion { get; set; }
+        public int Edad { get; set; }
+
+        
+
+    }
+}
